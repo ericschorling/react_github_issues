de8ea2fbc815f242715c3353f727bc29484892c4
Eric Schorling <eric.schorling@yahoo.com>
2020-10-13 11:46:33 -0400

    updated for markdown

--- src/components/IssueList.jsx
+++ src/components/IssueList.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import Issue from './Issue'
+import {Title} from 'bloomer'
 import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 import IssueDescription from './IssueDescription'
 export default class IssueList extends Component {
@@ -21,18 +22,24 @@ export default class IssueList extends Component {
       }
     render () {
         return (
-            <Router>
+          <Router>
             <Link to="/issues">Issues</Link>
             <Route exact path="/issues">
-            <ul>
-                {this.state.issues.map((issue, index)=>{
-                    return <li key={index}><Issue issue={issue}/></li>
-                    })
-                }
-            </ul>
+              <Title isSize={2}>Github Issues List</Title>
+              <ul>
+                  {!!this.state.issues.length ? this.state.issues.map((issue, index)=>{
+                      return <li key={issue.id}><Issue issue={issue}/></li>
+                      })
+                  : (
+                    <p>No Issues</p>
+                  )}
+              </ul>
             </Route>
             <Route path="/issues/:id">
                 <IssueDescription issues={this.state.issues}/>
+            </Route>
+            <Route path="*">
+              <h1>Oops nothing here</h1>
             </Route>
         </Router>
             
