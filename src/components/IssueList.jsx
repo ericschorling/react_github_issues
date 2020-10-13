75c1946c6ac557ca3a1f5c7e017546605e9a8e19
Eric Schorling <eric.schorling@yahoo.com>
2020-10-13 14:07:20 -0400

    refactor

--- src/components/IssueList.jsx
+++ src/components/IssueList.jsx
@@ -1,48 +1,44 @@
-import React, {Component} from 'react'
+import React, {useEffect, useState} from 'react'
 import Issue from './Issue'
-import {Title} from 'bloomer'
-import { BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
+import {Title, Container} from 'bloomer'
+import { BrowserRouter as Router, Route, Link} from "react-router-dom"
 import IssueDescription from './IssueDescription'
-export default class IssueList extends Component {
-    state={
-        issues: []
-      }
-    
-      getIssues = async () => {
-        const issues = await fetch ('https://api.github.com/repos/facebook/create-react-app/issues')
-        const response = issues.json()
-        return response
-      }
-      async componentDidMount () {
-        const issues = await this.getIssues()
-        console.log(issues)
-        this.setState({
-          issues: issues
-        })
-      }
-    render () {
-        return (
-          <Router>
-            <Link to="/issues">Issues</Link>
-            <Route exact path="/issues">
-              <Title isSize={2}>Github Issues List</Title>
-              <ul>
-                  {!!this.state.issues.length ? this.state.issues.map((issue, index)=>{
-                      return <li key={issue.id}><Issue issue={issue}/></li>
-                      })
-                  : (
-                    <p>No Issues</p>
-                  )}
-              </ul>
-            </Route>
-            <Route path="/issues/:id">
-                <IssueDescription issues={this.state.issues}/>
-            </Route>
-            <Route path="*">
-              <h1>Oops nothing here</h1>
-            </Route>
-        </Router>
-            
-        )
-    }
+import 'bulma/css/bulma.css'
+
+
+
+
+const IssueList = props => {
+  const [issues, setIssues] = useState([]);
+  useEffect(()=>  {
+    (async function () {
+      const response = await fetch ('https://api.github.com/repos/facebook/create-react-app/issues')
+      const issues = await response.json()
+      setIssues(issues)
+    })();
+  }, [setIssues])
+  return (
+    <Router>
+      <Link to="/issues">Issues</Link>
+      <Route exact path="/issues">
+        <Title isSize={2}>Github Issues List</Title>
+        <ul>
+            {!!issues.length ? issues.map((issue, index)=>{
+                return <li key={issue.id}><Issue issue={issue}/></li>
+                })
+            : (
+              <p>No Issues</p>
+            )}
+        </ul>
+      </Route>
+      <Route path="/issues/:id">
+          <IssueDescription issues={issues}/>
+      </Route>
+      <Route path="*">
+        <h1>Oops nothing here</h1>
+      </Route>
+  </Router>
+  )
 }
+
+export default IssueList
